762c9d4e6dbcba75a80f8aecc01d15f023c1e348
loading1031 <tjdans1031@gmail.com>
2023-12-01 10:34:06 +0900

    Chapter9 오류 수정

--- Loading/spring/spring/src/main/java/umc/study/service/StoreService/StoreQueryService.java
+++
@@ -1,4 +0,0 @@
-package umc.study.service.StoreService;
-
-public interface StoreQueryService {
-}
--- Loading/spring/spring/src/main/java/umc/study/service/StoreService/StoreQueryServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package umc.study.service.StoreService;
-
-public class StoreQueryServiceImpl {
-}
